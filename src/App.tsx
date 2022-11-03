9c2c4fe15142b591085c08917cb50bdfefed8be1
Vladimir Liankevich <liankevichvladimir@gmail.com>
2022-11-03 15:02:23 +0300

    change name for app

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return <div className="App">Hello world git </div>;
+  return <div className="App">bloggerPlatform </div>;
 }
 
 export default App;
