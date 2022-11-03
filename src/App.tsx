bbb861dce4fcdf01b06e8de296a7de5c1b17d8f9
Vladimir Liankevich <liankevichvladimir@gmail.com>
2022-11-03 15:00:00 +0300

    second test commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return <div className="App">Hello world</div>;
+  return <div className="App">Hello world git </div>;
 }
 
 export default App;
